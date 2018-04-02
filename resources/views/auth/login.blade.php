77c35c8bbabcea4525ce7c823e3d0ce6e4e60f2a
litvinovandrew <andleex@gmail.com>
2018-04-02 14:41:29 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,8 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">Login.</div>
+                <div class="panel-heading">Use login: test@test.com, password: 123123 </div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
